87b6b98e6bab4c59cb605356a61928e625175ef6
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-05-30 21:01:59 -0400

    state derivado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 export default function Header({ cart }) {
+  const isEmpty = () => cart.length === 0;
+
   return (
     <>
       <header className="py-5 header">
@@ -22,7 +24,7 @@ export default function Header({ cart }) {
                 />
 
                 <div id="carrito" className="bg-white p-3">
-                  {cart.length === 0 ? (
+                  {isEmpty() ? (
                     <p className="text-center">El carrito esta vacio</p>
                   ) : (
                     <table className="w-100 table">
